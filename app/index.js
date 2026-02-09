fdc776968e7358147908c6e1470f6bbafab01265
Daavidmp <cod.davidmp@gmail.com>
2026-02-09 14:42:12 +0100

    nuevo cambio

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Prueba para ver si funciona\n');
+    res.end('awawas\n');
 });
 server.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
