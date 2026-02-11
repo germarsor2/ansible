55fdec25ec6f86681efef1ea2c498f24358a9a65
Daavidmp <cod.davidmp@gmail.com>
2026-02-11 14:01:47 +0100

    arreglo

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('awawas\n');
+    res.end('Ejemplo mensaje\n');
 });
 server.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
